271e7cb0d4175634235c1d0a58610277bf81088a
xc308 <xc308@exeter.ac.uk>
2021-12-20 00:09:43 +0100

    armatoma

--- Chp3.R
+++ Chp3.R
@@ -12,5 +12,22 @@ plot(arima.sim(list(order = c(0, 0, 1), ma = -.9), n = 100),
 
 
 
+## ARMA(p, q)
+## Expl 3.7 Parameter Redundancy
+
+set.seed(07-15-2021) # Mars, I got your bday
+x <- rnorm(150, mean = 15)
+arima(x, order = c(1, 0, 1))
+
+
+## Example 3.12 Weights for an ARMA Model
+inf_MA_lst<- ARMAtoMA(ar = .9, ma = .5, 50) # convert ARMA to infinite MA
+plot(inf_MA_lst)
+
+
+
+
+
+
 
 
